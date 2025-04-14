89af2290245ab6679feb28ab05ebbf3eda22862c
Benjamin Metzger (RIT Student) <brm2167@rit.edu>
2025-04-14 11:54:00 +0200

    Finish FunWithThreads3

--- src/FunWithThreads3.java
+++ src/FunWithThreads3.java
@@ -1,12 +1,49 @@
+/*
+@ASSESSME.USERID: brm2167
+@ASSESSME.AUTHOR: Benjamin R. Metzger
+@ASSESSME.DESCRIPTION: PRACTICEPRACTICAL3
+@ASSESSME.ANALYZE: YES
+*/
+
 public class FunWithThreads3 {
+    private static int counter = 0;
 
+    /**
+     * Increments the counter by 1.
+     */
+    public synchronized void increment() {
+        counter++;
+    }
 
+    class Counter extends Thread {
+        /**
+         * Increments the counter 40,000 times.
+         */
+        @Override
+        public void run() {
+            for (int i = 0; i < 40000; i++) increment();
+        }
+    }
 
 
     //Constructor of FunWithThreads
     public FunWithThreads3(){
         System.out.println("MAIN START");
+
+        Thread counter1 = new Counter();
+        Thread counter2 = new Counter();
+
+        counter1.start();
+        counter2.start();
+
+        try {
+            counter1.join();
+            counter2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
      
+        System.out.println(counter);
         System.out.println("MAIN END");
     }
 
